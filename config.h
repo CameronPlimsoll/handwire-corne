804b71fbd36bc1b321abd96efb9c2579c014ed3c
Cameron Plimsoll <cameron.plimsoll@rapiddeploy.com>
2024-09-26 15:54:13 +0200

    update

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 // #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
 #define SERIAL_DEBUG
-#define SERIAL_USART_FULL_DUPLEX   // Enable half-duplex
-#define SERIAL_USART_TX_PIN GP4
-#define SERIAL_USART_RX_PIN GP5
-// #define SERIAL_USART_RX_PIN GP5  
-// #define SPLIT_LAYER_STATE_ENABLE
-// #define SPLIT_MODS_ENABLE
-// #define SPLIT_TRANSPORT_MIRROR
-// #define SERIAL_UART_TIMEOUT 100    // Timeout for UART operations (in ms)
-
+// #define SERIAL_USART_FULL_DUPLEX  
+#define SOFT_SERIAL_PIN GP4 // Enable half-duplex
+#define UART_RX_PIN GP4
+#define UART_TX_PIN GP5
+ #define SPLIT_LAYER_STATE_ENABLE
+ #define SPLIT_MODS_ENABLE
+ #define SPLIT_TRANSPORT_MIRROR
+ #define SERIAL_UART_TIMEOUT 100    // Timeout for UART operations (in ms)
+// #define SERIAL_USART_PIN_SWAP
  #define MASTER_LEFT
